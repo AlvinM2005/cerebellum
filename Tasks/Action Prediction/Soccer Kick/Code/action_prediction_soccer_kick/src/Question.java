a863bf415d27dfc42f1cbf4341a9b69e015e74c7
Amanda Chirino <amandachirino@MacBook-Pro-4.attlocal.net>
2025-07-27 22:19:13 -0700

    Delate Tasks folder

--- Tasks/Action Prediction/Soccer Kick/Code/action_prediction_soccer_kick/src/Question.java
+++
@@ -1,9 +0,0 @@
-public class Question {
-    String videoDir;
-    boolean answer;
-
-    public Question(String videoDir, boolean answer) {
-        this.videoDir = videoDir;
-        this.answer = answer;
-    }
-}
--- Tasks/mental_rotation/bidimensional_mental_rotation/Result.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-class Result(Enum):
-    # Define possible trial outcomes
-    Correct = "Correct"
-    Incorrect = "Incorrect"
-    Timeout = "Timeout"
